222ea6b09aa71a19b0ac99bd9c97ed890ed76d89
antonincoste <antonin@internetcompany.fr>
2025-08-11 17:47:06 +0200

    Implement full page loader

---
+++ src/components/FullPageLoader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './FullPageLoader.css'; // Nous allons créer ce fichier CSS juste après
+
+const FullPageLoader = ({ isLoading }) => {
+  if (!isLoading) {
+    return null;
+  }
+
+  return (
+    <div className="loader-overlay">
+      <div className="loader-spinner"></div>
+      <p style={{ color: 'white', marginTop: '20px', fontSize: '1.2em' }}>Processing updates, do not close or refresh the page...</p>
+    </div>
+  );
+};
+
+export default FullPageLoader;
